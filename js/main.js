217401bc423ef33a74a9dafd6a5efceff4cf639c
Amr Ahmed <aamrmosbah@gmail.com>
2023-06-04 14:53:21 +0300

    update data

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ let header = document.querySelector('.header');
 let img = document.querySelector('.img');
 
 function change(phone){
-    img.src = phone
+    img.src = phone ;
 }
 
 function color(changeColor){
-    header.style.background = changeColor
+    header.style.background = changeColor ;
 }
