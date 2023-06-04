c24e0626dc39cdce7486e8e1e8a588e0939534d5
Amr Ahmed <aamrmosbah@gmail.com>
2023-06-04 14:27:26 +0300

    Creaste this for apple

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let header = document.querySelector('.header');
+let img = document.querySelector('.img');
+
+function change(phone){
+    img.src = phone
+}
+
+function color(changeColor){
+    header.style.background = changeColor
+}
